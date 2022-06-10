a26b0d2c634cfbb9c93e28b075c5f38ea73c6fea
Subham Sharma <subhamengine@gmail.com>
2022-06-11 01:05:37 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import "./App.css";
 
-function App() {
-  return (
+export default function App() {
+  const [users, setUsers] = React.useState([]);
+  const f = async () => {
+    const res = await fetch("https://reqres.in/api/users?page=2/");
+    const json = await res.json();
+    setUsers(json.data);
+  };
+  React.useEffect(() => {
+    f();
+  }, []);
+  
+  const [singleUserData, setSingleUserData] = useState([])
+  
+  const setProfile = (id) => {
+    // console.log(id)
+    const s = async () => {
+      const res = await fetch(`https://reqres.in/api/users/${id}`);
+      
+      const json = await res.json();
+      // setUsers(json.data);
+      console.log(json);
+      setSingleUserData(json.data)
+    };
+    s();
+  }
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="display">
+        <img src={singleUserData.avatar} alt="" />
+        <h2>Name:  <span className="name"> {`${singleUserData.first_name} ${singleUserData.last_name}`} </span></h2>
+        <h3>User Info:- <span className="userInf">{singleUserData.email}</span></h3>
+      </div>
+      <div className="flex">
+        {users.length &&
+          users.map((user) => {
+            return (
+                <button className="btn" onClick={() => setProfile(user.id)}>{user.id}</button>
+            );
+          })}
+      </div>
     </div>
   );
 }
-
-export default App;
